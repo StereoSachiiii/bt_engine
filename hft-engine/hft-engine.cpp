b4005233a2b3930689766d454d73e56355782ba7
Sachin Lakshitha <niroshasulochini@gmail.com>
2026-02-09 19:39:19 +0530

    initial commit

---
+++ hft-engine/hft-engine.cpp
@@ -0,0 +1,12 @@
+﻿// hft-engine.cpp : Defines the entry point for the application.
+//
+
+#include "hft-engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
